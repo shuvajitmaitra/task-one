487378f695e7cfb17ef9a030b5d73bcd31daf44b
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2024-01-29 19:17:28 +0600

    tabs added

--- app/index.jsx
+++ app/index.jsx
@@ -1,8 +1,20 @@
-import { View, Text } from "react-native";
+import { Link, router } from "expo-router";
+import { View, Text, Pressable } from "react-native";
 export default function HomePage() {
   return (
     <View>
       <Text>HomePage</Text>
+      <Link href={"users/1"}>User Page 1</Link>
+      {/* <Pressable onPress={() => router.push("users/2")}>
+        <Text>User 2</Text>
+      </Pressable> */}
+      <Pressable 
+      onPress={()=> router.push({
+        pathname: 'users/[id]',
+        params: {id: 2}
+      })}>
+        <Text>User Page 2</Text>
+      </Pressable>
     </View>
   );
 }
